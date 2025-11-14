d5e1b35b3d5ffc9cf9c2f5368f807c65abfaff09
kavyashivaram19 <kavyas.cs24@bmsce.ac.in>
2025-11-14 11:56:16 +0530

    Add files via upload

---
+++ book/BookMain.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+class Book {
+    private String name;
+    private String author;
+    private double price;
+    private int num_pages;
+    public Book(String name, String author, double price, int num_pages) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.num_pages = num_pages;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setNumPages(int num_pages) {
+        this.num_pages = num_pages;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getNumPages() {
+        return num_pages;
+    }
+    public String toString() {
+        return "\nBook Details:" +"\nName: " + name +"\nAuthor: " + author +
+        "\nPrice: ₹" + price +"\nNumber of Pages: " + num_pages;
+    }
+}
+public class BookMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of books: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        Book[] books = new Book[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details of Book " + (i + 1) + ":");
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Author: ");
+            String author = sc.nextLine();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+            System.out.print("Number of Pages: ");
+            int num_pages = sc.nextInt();
+            sc.nextLine();
+            books[i] = new Book(name, author, price, num_pages);
+        }
+        System.out.println("\n--- Book Information ---");
+        for (int i=0;i<n;i++) {
+            System.out.println(books[i].toString());
+        }
+
+    }
+}
