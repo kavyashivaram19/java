242dae752979abf52b1b55ed97958f2e4b5b6ff1
kavyashivaram19 <kavyas.cs24@bmsce.ac.in>
2025-11-14 20:46:45 +0530

    Add files via upload

---
+++ quadratic/QuadraticRoots.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class QuadraticRoots {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter coefficient a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Enter coefficient b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Enter coefficient c: ");
+        double c = scanner.nextDouble();
+
+        double d= b * b - 4 * a * c;
+
+        System.out.println("Discriminant = " + d);
+
+        if (d> 0) {
+            double root1 = (-b + Math.sqrt(d)) / (2 * a);
+            double root2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println("Roots are real and distinct:");
+            System.out.println("Root 1 = " + root1);
+            System.out.println("Root 2 = " + root2);
+        } 
+        else if (d== 0) {
+            double root = -b / (2 * a);
+            System.out.println("Roots are real and equal:");
+            System.out.println("Root = " + root);
+        } 
+        else {
+            System.out.println("Roots are complex and imaginary:");
+        }
+    }
+}
