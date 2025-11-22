87f19efb1de50e2d442f32c5103ead68143a50d4
kavyashivaram19 <kavyas.cs24@bmsce.ac.in>
2025-11-22 12:04:36 +0530

    Add files via upload

---
+++ package/Internals.java
@@ -0,0 +1,10 @@
+package CIE;
+public class Internals 
+{
+    public int internalmarks[]=new int[5];
+    public Internals(int im[])
+    for(int i=0;i<5;i++)
+    {
+        this.internalmarks[i]=marks[i];
+    }
+}
