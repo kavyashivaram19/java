9d23a715423590d3b14d395f6bf1681ab03e935f
kavyashivaram19 <kavyas.cs24@bmsce.ac.in>
2025-11-26 17:24:58 +0530

    Add files via upload

--- package/External.java
+++ package/External.java
@@ -1,15 +1,15 @@
-package SEE;
-import CIE.Personal;
-public class External extends Personal
-{
-    public int externalmarks[]=new int[5];
-    public External(String usn,String name,int sem, int m[])
-    {
-        super(usn,name,sem);
-        for(int i=0;i<5;i++)
-        {
-            this.externalmarks[i]=marks[i];
+package see;
+import cie.Personal;
+import java.util.Scanner;
+
+public class External extends Personal {
+    public int seeMarks[] = new int[5];
+
+    public void readSEE() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 5 SEE Marks:");
+        for (int i = 0; i < 5; i++) {
+            seeMarks[i] = sc.nextInt();
         }
     }
-    
 }
