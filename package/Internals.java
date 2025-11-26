a0422dc21601a647439df415dc37912f8fa6fc33
kavyashivaram19 <kavyas.cs24@bmsce.ac.in>
2025-11-26 17:23:04 +0530

    Add files via upload

--- package/Internals.java
+++ package/Internals.java
@@ -1,10 +1,14 @@
-package CIE;
-public class Internals 
-{
-    public int internalmarks[]=new int[5];
-    public Internals(int im[])
-    for(int i=0;i<5;i++)
-    {
-        this.internalmarks[i]=marks[i];
+package cie;
+import java.util.Scanner;
+
+public class Internals {
+    public int internalMarks[] = new int[5];
+
+    public void readInternals() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 5 Internal Marks:");
+        for (int i = 0; i < 5; i++) {
+            internalMarks[i] = sc.nextInt();
+        }
     }
 }
